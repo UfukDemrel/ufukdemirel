616c8a02a9cee27c2150429632dfc1019b886e35
Ufuk Demirel <udemirel012@gmail.com>
2023-09-11 17:01:46 +0300

    index

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import music from '../images/music.png'; 
+import chat from '../images/chat.png'; 
 
 function Projects() {
   return (
@@ -14,11 +15,14 @@ function Projects() {
             </div>
         </a>
 
-        {/* <div className='pro-box'>
-            <div className='pro-title'>WAWE MUSİC WEBSİTE</div>
-            <img src={music} alt='alt'/>
-        </div>
-
+        <a href='https://chat-app-eta-opal.vercel.app/' target='_blank' rel="noreferrer">
+          <div className='pro-box'>
+              <div className='pro-title'>Chat App</div>
+              <img src={chat} alt='alt'/>
+          </div>
+        </a>
+ 
+      {/* 
         <div className='pro-box'>
             <div className='pro-title'>WAWE MUSİC WEBSİTE</div>
             <img src={music} alt='alt'/>
