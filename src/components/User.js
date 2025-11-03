16ea81475fdba11123d4c9310053d6549acabe8b
UfukDemrel <126552914+UfukDemrel@users.noreply.github.com>
2025-11-03 18:40:52 +0300

    fix6

--- src/components/User.js
+++ src/components/User.js
@@ -8,12 +8,12 @@ function User() {
               <div className='user-bio-p drop-in'>Hello!</div>
               <div className='img-text'>
                 <img src={profileImage} className='user-bio-img grow rounded-full md:rounded-2xl' alt="alt"/>
-                <p className='user-bio-text drop-in-2'>l'm' a Frontend Developer shaping the web with code and design.</p>
+                <p className='user-bio-text drop-in-2'>l'm' a Web & Shopify Developer shaping the web with code and design.</p>
               </div>
-              <div className='quetes'>
+              {/* <div className='quetes'>
                 <img src='tırnak.png' alt='alt'/>
                 <p>If we stop here and go no further, why have we come this far?</p>
-              </div>
+              </div> */}
           </div>
       </div>
   )
